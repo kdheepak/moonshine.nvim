ef05478897d5b8fbd4e35ab15ccce076ca9394f6
Dheepak Krishnamurthy <me@kdheepak.com>
2021-08-05 22:50:54 -0600

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+use mlua::prelude::*;
+
+fn hello(_: &Lua, name: String) -> LuaResult<()> {
+    println!("hello, {}!", name);
+    Ok(())
+}
+
+#[mlua::lua_module]
+fn my_module(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+    exports.set("hello", lua.create_function(hello)?)?;
+    Ok(exports)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
